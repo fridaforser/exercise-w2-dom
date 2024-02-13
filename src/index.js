5e46c12fe8d7197b3d8bb3ea669c4e91d0432003
fridaforser <fridaforser@gmail.com>
2024-02-13 13:40:34 +0100

    Co-authored-by: AHPIXI <AHPIXI@users.noreply.github.com> Co-authored-by: Erika Wernbro <Tejpex@users.noreply.github.com> Co-authored-by: Alexandra Franzén <Xikitita@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log("Start Here 😉");
+const radioDark = document.getElementById("dark")
+const radioLight = document.getElementById("light")
+const radioCrazy = document.getElementById("crazy")
+const box = document.getElementById("box")
+const boxText = document.getElementById("boxText")
+
+radioDark.onclick = () => {
+    box.style.backgroundColor = "black"
+    boxText.style.color = "white"
+}
+radioLight.onclick = () => {
+    box.style.backgroundColor = "lightgreen"
+    boxText.style.color = "black"
+}
+radioCrazy.onclick = () => {
+    box.style.backgroundColor = "red"
+    boxText.style.color = "yellow"
+}
+
